b0a87089e24d1590b5fc7d761419c515947c1f71
Harvey <94400002+HarveyEsteban@users.noreply.github.com>
2024-09-07 18:00:37 +0800

    Added Login page Design and Readme tutorials how to install tailwind Css

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # MobsSystemFIles
 Mobs System Files, All Changes Updates Should be stored in a Github Repo
+
+----------------------------------------------------------------------------------
+#How to run and Install tailwind CSS
+note: This Webpage uses tailwind CSS framework for its Stylesheet
+----------------------------------------------------------------------------------
+Step 1 : "npm init -y" to start and initialize npm.
+Step 2 : Configure tailwind css.
+        "@tailwind base;
+         @tailwind components;
+         @tailwind utilities;" //put this in the Input.css
+
+         "./src/pages/*.{php,html,js}" //put this in the tailwind.config [content].
+
+         note: to get tailwindconfig.js run command - "npx tailwindcss init"
+
+        -install tailwind using "npm install -D tailwindcss"
+Step 3: Go to package.json and add this script:
+                     "tailwind:css": "tailwindcss -i ./src/css/input.css -o ./src/css/output.css --watch"
+Step 4: link the css file "output.css" to your file.
+Step 5: run the script you added "npm run tailwind:css"
+                                 note: tailwind:css is the name of the Script
+----------------------------------------------------------------------------------
--- src/pages/loginPage.php
+++ src/pages/loginPage.php
@@ -44,15 +44,40 @@
     <!-- Tailwind Css Link -->
      <link rel="stylesheet" href="../css/output.css">
 </head>
-<body class=">
-    <div class="container">
-        <form method="post">
-            <Label>Enter Mobs ID</Label>
-            <input type="text" name="mobsID" class="mobsID">
-            <label>Enter Password</label>
-            <input type="password" name="mobsPass" class="mobsPass">
-            <button type="submit" name="submitBtn" class="submitBtn">Submit</button>
-        </form>
+<body class="flex flex-col bg-white h-screen bg-cover bg-center bg-no-repeat bg-mobs-bg">
+
+    <div class="flex justify-center bg-slate-500 items-center mb-20 p-5">
+        <img class="w-20 mr-1" src="../img/logo.png" alt="mbos-logo">
+        <h2 class="text-5xl italic font-bold text-black">Employee Portal</h2>
     </div>
+
+    <div class="flex items-center justify-center">
+    <form method="post">
+
+            <div class="flex flex-col gap-2 w-120 bg-slate-400 bg-opacity-80 p-20 pb-32 rounded-2xl ">
+
+            <div class="flex mb-5 bg-white rounded-lg p-2">
+            <Label class=" text-lg text-center font-bold bg-transparent font- p-2">Enter Mobs ID</Label>
+            <input type="text" name="mobsID" class="mobsID bg-transparent text-center text-lg flex-auto w-52">
+            </div>
+
+            <div class="flex mb-5 bg-white rounded-lg p-2">
+            <label class=" flex-grow text-lg text-center bg-transparent font-bold p-2 rounded-">Enter Password</label>
+            <input type="password" name="mobsPass" class="mobsPas bg-transparent text-center flex-initial w-52 ">
+            </div>
+
+            <button type="submit" name="submitBtn" class="submitBtn bg-red-500  rounded-md p-2 font-bold flex-1">Login</button>
+
+            </div>
+        </form>
+
+    </div>
+
+    <div class="flex justify-center items-center bg-slate-500 mt-auto p-5">
+        <h1 class="text-4xl italic font-bold text-black">Est. 2013</h1>
+    </div>
+
+
+       
 </body>
 </html>
